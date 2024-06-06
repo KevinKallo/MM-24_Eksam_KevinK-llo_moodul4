83243f6f61f24bc3c2d5bfca3a879c4f09b62a6f
KevinKallo <kevin.kallo@tptlive.ee>
2024-06-06 16:08:59 +0200

    first commit

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,28 @@
+<header class="header">
+                <img src="logod/SF1.png" alt="Logo" class="logo">
+                <nav class="nav">
+                <a href="Index2.html" class="nav-link">Home</a>
+                <a href="epood.html" class="nav-link">E-shop</a>
+                <a href="forms.html" class="nav-link">Follow</a>
+                <a href="#" class="nav-link">Contact</a>
+                </nav>
+                <div class="navbtn">
+                    <div class="menu-toggle">
+                        <svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="14.5" cy="14.5" r="13" stroke="white" stroke-width="3"/>
+                        </svg>
+                    </div>
+                    <div class="Profile">
+                        <svg width="22" height="30" viewBox="0 0 22 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="10.9701" cy="8.04478" r="6.54478" stroke="white" stroke-width="3"/>
+                            <path d="M1.5 26.694C1.5 21.6658 5.57622 17.5895 10.6045 17.5895C15.6328 17.5895 19.709 21.6658 19.709 26.694V27.7537H1.5V26.694Z" stroke="white" stroke-width="3"/>
+                        </svg>                    
+                    </div>
+                    <div class="Search">
+                        <svg width="22" height="30" viewBox="0 0 22 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M19.709 11.0658C19.709 16.4097 15.5732 20.6316 10.6045 20.6316C5.63577 20.6316 1.5 16.4097 1.5 11.0658C1.5 5.72192 5.63577 1.5 10.6045 1.5C15.5732 1.5 19.709 5.72192 19.709 11.0658Z" stroke="white" stroke-width="3"/>
+                            <path d="M14.627 19.0789L20.112 29" stroke="white" stroke-width="3"/>
+                        </svg>                                        
+                    </div>
+                </div>
+            </header>
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('shop', function () {
+    return view('shop');
+});
+
+Route::get('forms', function () {
+    return view('forms');
+});
+
+Route::get('nav', function () {
+    return view('nav', [
+        'nav' => 'Hello World'
+    ]);
+});
+
